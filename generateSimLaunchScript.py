d830c2a3b7b5504ed8841e517f82b4bb6f9f7fa3
Murillo Fernando Rodrigues <murillofer.rodrigues@gmail.com>
2018-10-16 13:58:03 -0700

    minor bugs correction (#11)
    * minor bugs correction
    
    * removed -i

--- generateSimLaunchScript.py
+++ generateSimLaunchScript.py
@@ -2,7 +2,7 @@ import sys, os
 """
 python generateSimLaunchScript.py
 This script emits a bash launch script for running all of the training/testing simulations required by discoal
-usage example: python generateSimLaunchScript.py popsizehist.txt train/ test/ > launch_script.sh
+usage example: python generateSimLaunchScript.py popsizehist.txt train test > launch_script.sh
 
 You can then run `bash launch_script.sh` in order to run the simulations;  if they take too long then you 
 will have to do them in batches on a cluster.
@@ -24,15 +24,13 @@ trainingOutDir, testOutDir = sys.argv[2:]
 # alpha/rho may have to be adjusted in order to achieve this
 trainingSampleNumber = 2000 #the number of simulation replicates we want to generate for each file in our training set
 testSampleNumber = 1000 #the number of simulations to create for each file in the test set
-sampleSize = 10 #the number of individuals in our population sample
+sampleSize = 100 #the number of individuals in our population sample
 numSites = 55000 #total number of sites in our simulated window (i.e. S/HIC's subwindow size * 11)
 u = 3.5e-9 #per-site mutation rate (used to calculate mean theta)
 gensPerYear = 11.0 #number of generations per year
 maxSoftSweepInitFreq = 0.1 #maximum initial selected frequency for soft sweeps
 tauHigh = 0.05 #maximum FIXATION (not mutation) time (in units of 4N generations ago) in the past
 rhoOverTheta = 5.0 #crossover rate over mut rate (used to calculate mean rho)
-alphaHigh = 2*N0*0.005 # max selection coefficient s is 0.005
-alphaLow = 2*N0*0.0001 # mininum selection coefficient s at 0.0001
 
 sizeChanges = []
 with open(popSizeFileName) as popSizeFile:
@@ -57,6 +55,9 @@ thetaLow = (2*thetaMean)/11.0
 thetaHigh = 10*thetaLow
 rhoMax = 3 * rhoMean
 
+alphaHigh = 2*N0*0.005 # max selection coefficient s is 0.005
+alphaLow = 2*N0*0.0001 # mininum selection coefficient s at 0.0001
+
 selStr = " -ws 0 -Pa %f %f -Pu 0 %f" %(alphaLow, alphaHigh, tauHigh)
 partialSelStr = " -ws 0 -Pa %f %f" %(alphaLow, alphaHigh)
 softStr = " -Pf 0 %f" %(maxSoftSweepInitFreq)
@@ -71,7 +72,7 @@ print("#!/bin/bash")
 for sampleNumber, outDir, simTitle in [(trainingSampleNumber, trainingOutDir, "training data"), (testSampleNumber, testOutDir, "test data")]:
     partialStr = "-Pc 0.2 0.99"
     print("\n#generating %s\n" %(simTitle))
-    neutDiscoalCmd = "discoal -i 4 %d %d %d -Pt %f %f -Pre %f %f%s" %(sampleSize, sampleNumber, numSites, thetaLow, thetaHigh, rhoMean, rhoMax, demogStr)
+    neutDiscoalCmd = "discoal %d %d %d -Pt %f %f -Pre %f %f%s" %(sampleSize, sampleNumber, numSites, thetaLow, thetaHigh, rhoMean, rhoMax, demogStr)
     print("%s > %s/Neut.msOut" %(neutDiscoalCmd, outDir))
     print("i=0")
     print("for x in 0.045454545454545456 0.13636363636363635 0.22727272727272727 0.3181818181818182 0.4090909090909091 0.5 0.5909090909090909 0.6818181818181818 0.7727272727272727 0.8636363636363636 0.9545454545454546;\ndo")
