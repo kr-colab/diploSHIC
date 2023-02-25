7cfb9ba8f0192ed8a1a77c737de784d7e4cbca0c
Andrew Kern <adkern@uoregon.edu>
2023-02-24 16:02:12 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ shic_stats = Extension("diploshic.shicstats",
                                 "diploshic/utils.c"],
                       )
 setup(name='diploSHIC',
-      version='0.333333',
+      version='0.4',
       description='diploSHIC',
       long_description=long_description,
       long_description_content_type="text/markdown",
